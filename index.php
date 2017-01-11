5cb7801ad2d269c6601d91f26a5e681f202f9790
Yang Sun (孙洋) <Yang.Sun@dentsu.com.cn>
2017-01-11 14:31:00 +0800

    Signed-off-by: Yang Sun

--- index.php
+++ index.php
@@ -18,9 +18,8 @@ if (version_compare(PHP_VERSION, '5.3.0', '<')) {
 
 // 开启调试模式 建议开发阶段开启 部署阶段注释或者设为false
 define('APP_DEBUG', true);
-define('USERNAME', 'SSFSD');
+
 // 定义应用目录
-echo USERNAME;
 define('APP_PATH', './Application/');
 
 // 引入ThinkPHP入口文件
